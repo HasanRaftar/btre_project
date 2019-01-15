4a92194dc1ee57ed08707ecba1b3b22329c17bc9
Hasan Raftar <ithasan7@gmail.com>
2019-01-16 01:08:24 +0600

    static

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Listing
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tittle', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+
 admin.site.register(Listing)
